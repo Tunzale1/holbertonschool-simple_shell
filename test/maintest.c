c431ecab52c68a622545d0b976578a1788bd2396
Haji <hajimursalzade@gmail.com>
2023-12-25 11:59:31 -0800

    test

---
+++ test/maintest.c
@@ -0,0 +1,14 @@
+#include "testmain.h"
+
+/**
+ * q
+ *aaa
+ *
+ *
+ */
+
+#define MAX_COMMAND_LENGTH 100
+
+int main()
+{
+}
---
+++ test/maintest.h
@@ -0,0 +1,14 @@
+#ifndef testMain
+#define testMain
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#endif
